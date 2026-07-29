0d8dddcd1debc1710661d5624f2e3fbf622ed913
Rehaan Ahmad <rehaanahmad2013@gmail.com>
2026-07-29 10:52:41 -0700

    Ablate Relay teacher leg to five paragraphs

--- reproduction/model_eval.py
+++ reproduction/model_eval.py
@@ -144,7 +144,7 @@ def launch_shards(
                     "--max_takeovers",
                     "2",
                     "--paragraphs_per_takeover",
-                    "3",
+                    "5",
                     "--save_rollout_trace",
                     "--trace_mode",
                     "mask",
